6220a45c82c4b6734b446153af483715098437e8
farahraihan <raihanunnisafarah@gmail.com>
2024-06-20 08:28:28 +0700

    submit fitur add detail transaksi

--- internal/controllers/detail_transaksi.go
+++ internal/controllers/detail_transaksi.go
@@ -1,13 +1,33 @@
 package controllers
 
-import "tokoku/internal/models"
+import (
+	"fmt"
+	"tokoku/internal/models"
+)
 
 type DetailTransaksiController struct {
-	model *models.DetailTransaksiModel
+	model            *models.DetailTransaksiModel
+	barangController *BarangController
 }
 
-func NewDetailTransaksiController(m *models.DetailTransaksiModel) *DetailTransaksiController {
+func NewDetailTransaksiController(m *models.DetailTransaksiModel, bc *BarangController) *DetailTransaksiController {
 	return &DetailTransaksiController{
-		model: m,
+		model:            m,
+		barangController: bc,
 	}
+}
+
+func (dc *DetailTransaksiController) AddDetailTransaksi(transaksiID, barangID uint, jumlah uint, harga float64) error {
+	// Kurangi stok barang
+	err := dc.barangController.DecreaseStock(barangID, jumlah)
+	if err != nil {
+		return fmt.Errorf("failed to decrease stock: %w", err)
+	}
+
+	// Tambah detail transaksi ke database
+	err = dc.model.AddDetailTransaksi(transaksiID, barangID, jumlah, harga)
+	if err != nil {
+		return fmt.Errorf("failed to add detail transaksi: %w", err)
+	}
+	return nil
 }
